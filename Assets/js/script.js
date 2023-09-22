3e8003f90d40ad3d49e510c679e8ee513fdbb593
b0n3yard <namesake39@gmail.com>
2023-09-22 15:06:33 -0400

    cleaning up comments and extrainious code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,77 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
- var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var swdate = $('#currentDay')
 var svmsg = $('.svmsg')
 var nine = $('#hour-9')
 var test = $('.hi')
-// var test2 = $('.hi').find('.testing9')
 var btn = $('button')
 var date = dayjs().date()
 var day = dayjs().hour()
-console.log(day)
 var newday = new Date()
 var mnth = months[newday.getMonth()];
-console.log(newday)
 var count = 0;
-console.log(mnth + ' '+ date)
-// console.log (mnth)
-swdate.append(mnth + ' '+ date);
+swdate.append(mnth + ' ' + date);
 
 
 
-for(x = 8; x < 17; x++){
-var num = x + 1;
-var str = 'hour-';
-var wish = 'testing';
-var nmstr = num.toString();
-var com2 = wish + nmstr;
-var com = str + nmstr;
-count = count +1;
-console.log(count)
-// var test = $('.hi')
-if(num < day){
-  test.append('<div class = "row time-block past '+ com2 + '" '+'id = "'+ com+ '" >')
-}else if( num > day){
-  test.append('<div class = "row time-block future '+ com2 + '" '+'id = "'+ com+ '">')
-}else if(num === day){
-  test.append('<div class = "row time-block present '+ com2 + '" '+'id = "'+ com+ '">')
-}
-var tweb = $('.'+com2);
-if (num > 12){
-  var nwnm = num - 12;
-  tweb.append('<div class = " col-2 col-md-1 hour text-center py-3">' + nwnm + "PM"+'</div>');
-}else{
-  tweb.append('<div class = " col-2 col-md-1 hour text-center py-3">' + nmstr + "AM"+'</div>');
-}
+for (x = 8; x < 17; x++) {
+  var num = x + 1;
+  var str = 'hour-';
+  var wish = 'testing';
+  var nmstr = num.toString();
+  var com2 = wish + nmstr;
+  var com = str + nmstr;
+  count = count + 1;
+  if (num < day) {
+    test.append('<div class = "row time-block past ' + com2 + '" ' + 'id = "' + com + '" >')
+  } else if (num > day) {
+    test.append('<div class = "row time-block future ' + com2 + '" ' + 'id = "' + com + '">')
+  } else if (num === day) {
+    test.append('<div class = "row time-block present ' + com2 + '" ' + 'id = "' + com + '">')
+  }
+  var tweb = $('.' + com2);
+  if (num > 12) {
+    var nwnm = num - 12;
+    tweb.append('<div class = " col-2 col-md-1 hour text-center py-3">' + nwnm + "PM" + '</div>');
+  } else {
+    tweb.append('<div class = " col-2 col-md-1 hour text-center py-3">' + nmstr + "AM" + '</div>');
+  }
 
-tweb.append('<textarea class = "col-8 col-md-10 description"' + 'rows = "3">');
-var amndtxt = localStorage.getItem( com)
-var set = $('#' + com + ' ' + 'textarea')
-set.val(amndtxt)
-tweb.append('<button class = "btn saveBtn col-2 col-md-1 ending"' + 'aria-label = "save">');
-// tweb.append($('section'),'class = " col-2 col-md-1 hour text-center py-3">' + nmstr + "AM");
+  tweb.append('<textarea class = "col-8 col-md-10 description"' + 'rows = "3">');
+  var amndtxt = localStorage.getItem(com)
+  var set = $('#' + com + ' ' + 'textarea')
+  set.val(amndtxt)
+  tweb.append('<button class = "btn saveBtn col-2 col-md-1 ending"' + 'aria-label = "save">');
 }
 
 var num = x + 1;
@@ -79,32 +49,15 @@ var tweb2 = $('.ending');
 tweb2.append('<i class= "fas fa-save"' + 'aria-hidden = "true">');
 var test2 = $('.hi').find('button')
 
-function savfnctin(e){
-  
-  // var textarea = test2.prev();
-  // var hlp = textarea.find('.description')
-  // console.log(hlp)
-  // console.log(text)
-  var des = $('#' + e.target.parentElement.id + ' '+ 'textarea')
+function savfnctin(e) {
+  var des = $('#' + e.target.parentElement.id + ' ' + 'textarea')
   var text = des.val();
- 
-  console.log(text);
-  // console.log(textarea)
-    var hly = localStorage.setItem(e.target.parentElement.id,  text )
-    console.log(e.target.parentElement.id)
-    console.log(e.target.id)
-    svmsg.append('<p>'+ 'saved to localstorage' +'</p>')
-    // var newtxt = 'job done';
-    // des.val(newtxt);
-    
+  var hly = localStorage.setItem(e.target.parentElement.id, text)
+  svmsg.append('<p>' + 'saved to localstorage' + '</p>')
 
 }
-// tweb.append('<button class = "btn saveBtn col-2 col-md-1 ending"' + 'aria-label = "save">');
 
 
 
 console.log(count)
 test2.on('click', savfnctin)
-// nine.removeClass('past');
-// nine.addClass('future')
-// .attr('id', 'hour-5');
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# 05_module_5
+# Daily Weekday Planner
+
+# Description
+Well this was an interesring puzzle. targeting dynamic elements was a lot of work, but a ton of fun.
+
+## Installation
+to insall this project, clone the repository from [github](https://github.com/b0n3yard/Daily_Weekday_Planner) and open index.html in the chrome browser. alternatively you can view the finished page [here](https://b0n3yard.github.io/Daily_Weekday_Planner/)
+
+
+## Usage
+
+![Alt text]( ./Assets/planner-before.png "example")
+![Alt text]( ./Assets/planner-after.png "example")
+
+
+## Credits
+Michael Klein
+A lot of help from [W3schools](https://www.w3schools.com/js/default.asp)
+and a little advice from our robot overlords.
+
+## Licence
